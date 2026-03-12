6cf5c74f36a15b88ab52c61872409ea936089a7a
miey425 <satomiyabi0425@gmail.com>
2026-03-12 14:37:16 +0900

    test

--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -3,7 +3,7 @@ import { NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
   const body = await req.text();
 
-  console.log("Webhook received:", body);
+  console.log("Stripe webhook received:", body);
 
   return new Response("ok", { status: 200 });
 }
